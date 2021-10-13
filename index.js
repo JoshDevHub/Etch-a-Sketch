891d8d0cf17f543813d7326bca9cac2c18849870
jmsmith1018 <jmsmith1018@gmail.com>
2021-10-13 10:55:13 -0600

    Add mouseover & mouseout events to grid-items for hover effect

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 const gridContainer = document.querySelector('.grid-container');
 
+const itemMouseoverHandler = (event) => {
+  event.target.style['background-color'] = 'gray';
+};
+
+const itemMouseoutHandler = (event) => {
+  event.target.style['background-color'] = '';
+};
+
 const createItems = () => {
   const itemArray = [];
   for (let i = 0; i < 256; i++) {
     let newItem = document.createElement('div');
-    newItem.classList.add('grid-container__item')
+    newItem.addEventListener('mouseover', itemMouseoverHandler);
+    newItem.addEventListener('mouseout', itemMouseoutHandler);
+    newItem.classList.add('grid-container__item');
     itemArray.push(newItem);
   }
   return itemArray;
-}
+};
 
 const populateGrid = (gridItemArray) => {
   for (let i = 0; i < gridItemArray.length; i++) {
     gridContainer.appendChild(gridItemArray[i]);
   }
-}
+};
 
 gridContainer.style['grid-template-columns'] = 'repeat(16, 1fr)';
 const items = createItems();
