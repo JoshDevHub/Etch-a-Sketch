173d8a531418940c37379ed3f66acc70dcf87457
jmsmith1018 <jmsmith1018@gmail.com>
2021-10-13 12:24:24 -0600

    Add functionality for reset button, allowing the user to reset the grid and select a new grid size

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 const gridContainer = document.querySelector('.grid-container');
 
+let gridSize = 16;
+
+const validateGridSize = (size) => {
+  let sizeToCheck = size;
+  while (
+    !Number.isInteger(sizeToCheck) ||
+    sizeToCheck < 16 ||
+    sizeToCheck > 100
+  ) {
+    sizeToCheck = parseInt(
+      prompt(
+        "You've entered an invalid size. Please enter a value between 16 and 100"
+      )
+    );
+  }
+  gridSize = sizeToCheck % 2 === 0 ? sizeToCheck : sizeToCheck - 1;
+};
+
 const itemMouseoverHandler = (event) => {
   event.target.style['background-color'] = 'gray';
 };
@@ -8,9 +26,10 @@ const itemMouseoutHandler = (event) => {
   event.target.style['background-color'] = '';
 };
 
-const createItems = () => {
+const createItems = (sideSize) => {
+  const totalItems = sideSize ** 2;
   const itemArray = [];
-  for (let i = 0; i < 256; i++) {
+  for (let i = 0; i < totalItems; i++) {
     let newItem = document.createElement('div');
     newItem.addEventListener('mouseover', itemMouseoverHandler);
     newItem.addEventListener('mouseout', itemMouseoutHandler);
@@ -24,8 +43,20 @@ const populateGrid = (gridItemArray) => {
   for (let i = 0; i < gridItemArray.length; i++) {
     gridContainer.appendChild(gridItemArray[i]);
   }
+  gridContainer.style['grid-template-columns'] = `repeat(${gridSize}, 1fr)`;
 };
 
-gridContainer.style['grid-template-columns'] = 'repeat(16, 1fr)';
-const items = createItems();
+const items = createItems(gridSize);
 populateGrid(items);
+
+const resetButton = document.querySelector('.reset-button');
+
+const resetBtnClickHandler = () => {
+  gridContainer.replaceChildren();
+  gridSize = parseInt(prompt('Enter a new size between 16 and 100'));
+  validateGridSize(gridSize);
+  const newItems = createItems(gridSize);
+  populateGrid(newItems);
+};
+
+resetButton.addEventListener('click', resetBtnClickHandler);
