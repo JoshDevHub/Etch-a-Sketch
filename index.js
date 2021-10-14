7688af81e857f715b2665d208cd55e210dbf315d
jmsmith1018 <jmsmith1018@gmail.com>
2021-10-14 10:33:15 -0600

    Refactor and reorganize existing code

--- index.js
+++ index.js
@@ -1,64 +1,46 @@
-const gridContainer = document.querySelector('.grid-container');
+// Logic & Event Handlers for Drawing on the Canvas
+const randomColorButton = document.querySelector('.random-mode');
+const colorInput = document.querySelector('input');
 
-let gridSideSize = 16;
+let drawColor = 'gray';
 
-const validateGridSize = (size) => {
-  let sizeToCheck = size;
-  while (
-    !Number.isInteger(sizeToCheck) ||
-    sizeToCheck < 16 ||
-    sizeToCheck > 100
-  ) {
-    sizeToCheck = parseInt(
-      prompt(
-        "You've entered an invalid size. Please enter a value between 16 and 100"
-      )
-    );
-  }
-  gridSideSize = sizeToCheck % 2 === 0 ? sizeToCheck : sizeToCheck - 1;
+const updateColor = (event) => (drawColor = event.srcElement.value);
+
+colorInput.addEventListener('change', updateColor);
+
+const toggleRandomColorMode = () => {
+  randomColorButton.classList.toggle('random-mode--true');
 };
+
+randomColorButton.addEventListener('click', toggleRandomColorMode);
 
 const getRandomHSLColor = () => {
   const randomHue = Math.floor(Math.random() * 360);
   return `hsl(${randomHue}, 50%, 50%)`;
-}
+};
 
-let drawColor = 'gray';
-let randomColorMode = false;
-
-const randomColorButton = document.querySelector('.random-mode');
-
-const toggleRandomColorMode = () => {
-  randomColorMode = !randomColorMode;
-  randomColorButton.classList.toggle('random-mode--true');
-}
-
-randomColorButton.addEventListener('click', toggleRandomColorMode);
-
-const updateColor = (event) => {
-  drawColor = event.srcElement.value;
-}
-
-const colorInput = document.querySelector('input');
-colorInput.addEventListener('change', updateColor);
-
-const itemMouseoverHandler = (event) => {
-  if (randomColorMode) {
+const drawOnMouseoverHandler = (event) => {
+  if (randomColorButton.classList.contains('random-mode--true')) {
     event.target.classList.add('grid-container__item--drawn');
     event.target.style.setProperty('--draw-color', getRandomHSLColor());
   } else {
-    event.target.classList.add('grid-container__item--drawn')
+    event.target.classList.add('grid-container__item--drawn');
     event.target.style.setProperty('--draw-color', drawColor);
   }
 };
+
+// Logic & Event Handlers for Creating and Reseting the Grid
+const gridContainer = document.querySelector('.grid-container');
+
+let gridSideSize = 16;
 
 const createItems = (sideSize) => {
   const totalItems = sideSize ** 2;
   const itemArray = [];
   for (let i = 0; i < totalItems; i++) {
     let newItem = document.createElement('div');
-    newItem.addEventListener('mouseover', itemMouseoverHandler, {
-      once: true
+    newItem.addEventListener('mouseover', drawOnMouseoverHandler, {
+      once: true,
     });
     newItem.classList.add('grid-container__item');
     itemArray.push(newItem);
@@ -77,6 +59,22 @@ const items = createItems(gridSideSize);
 populateGrid(items);
 
 const resetButton = document.querySelector('.reset-button');
+
+const validateGridSize = (size) => {
+  let sizeToCheck = size;
+  while (
+    !Number.isInteger(sizeToCheck) ||
+    sizeToCheck < 16 ||
+    sizeToCheck > 100
+  ) {
+    sizeToCheck = parseInt(
+      prompt(
+        "You've entered an invalid size. Please enter a value between 16 and 100"
+      )
+    );
+  }
+  gridSideSize = sizeToCheck % 2 === 0 ? sizeToCheck : sizeToCheck - 1;
+};
 
 const resetBtnClickHandler = () => {
   gridContainer.replaceChildren();
